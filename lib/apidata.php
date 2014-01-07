0e8a3543e4795e185c486186fb596e152c3e09d8
Joseph Russell <jrussell@redtettemer.com>
2014-01-07 15:05:15 -0500

    Adding apidata class file

---
+++ lib/apidata.php
@@ -0,0 +1,10 @@
+<?php
+
+/* This file exists as an abstraction of your sensitive API key information. You can find
+   these keys at https://foursquare.com/developers/apps */
+class apidata {
+  protected static $client_key = 'YOUR CLIENT ID HERE';
+  protected static $client_secret = 'YOUR CLIENT SECRET HERE';
+}
+
+?>
